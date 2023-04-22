b11db8c6a0ee17c944f638559773e2f0caf98d40
Wojciech Gajda <wojciechgajda@Wojciechs-MacBook-Pro.local>
2023-04-22 18:50:44 +0200

    add selection character screen

--- characterSelection.ts
+++ characterSelection.ts
@@ -1,3 +1,18 @@
+const startButton = document.getElementById("start-button");
+const loginScreen = document.getElementsByClassName(
+  "login-screen"
+)[0] as unknown as HTMLElement;
+startButton?.addEventListener("click", () => {
+  loginScreen.style.display = "none";
+  const audio = new Audio("./audio/login-screen-music-pack.mp3");
+  audio.play();
+  console.log(audio);
+  audio.loop = true;
+  audio.autoplay = true;
+  audio.controls = true;
+  audio.volume = 0.5;
+});
+
 const femaleWarriorChoose = document.getElementById("female-warrior");
 const femaleArcherChoose = document.getElementById("female-archer");
 const maleWarriorChoose = document.getElementById("male-warrior");
